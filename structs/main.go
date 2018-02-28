a84051e849cffeb6355f7d89f28ffaa445f67db3
Sean Lester <slester@spark.net>
2018-02-28 17:41:13 +0100

    Structs

---
+++ structs/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	// Method 1:
+	// alex := person{"Alex", "Anderson"}
+
+	// Method 2:
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	// Method 3:
+	// var alex person
+
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+// Turn address (&) into a value with *address
+// Turn value into address with &value
