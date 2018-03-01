d00458712d7b69f5ea3c5a3697af0dd7b16aa38b
Sean Lester <slester@spark.net>
2018-03-01 12:46:38 +0100

    Map section finished

--- map/main.go
+++ map/main.go
@@ -6,7 +6,14 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
